8fe52596bea7ef6501d1df6d395b5601528d1541
Juan Maria Pacheco <jmpacheco.dev@gmail.com>
2022-03-30 11:34:07 -0300

    Añadi el index.html con links al resto de documentos

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # platzi-curso-practico-javascript
+
+...
+
+## Taller #1: figuras geométricas
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
+
+## Taller #2: porcentajes y descuentos
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
+
+## Taller #3: promedio, mediana y moda
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+  name: "Juanita",
+  salary: 500,
+});
+colombia.push({
+  name: "Natalia",
+  salary: 1400,
+});
+colombia.push({
+  name: "Claudia",
+  salary: 1500,
+});
+colombia.push({
+  name: "Ricardo",
+  salary: 1300,
+});
+colombia.push({
+  name: "Luisa",
+  salary: 2000,
+});
+colombia.push({
+  name: "Sofia",
+  salary: 2100,
+});
+colombia.push({
+  name: "Roman",
+  salary: 1700,
+});
+colombia.push({
+  name: "Claudio",
+  salary: 1800,
+});
+colombia.push({
+  name: "Gonzalo",
+  salary: 2700,
+});
+colombia.push({
+  name: "Diego",
+  salary: 2400,
+});
+colombia.push({
+  name: "Jonathan",
+  salary: 2378,
+});
+colombia.push({
+  name: "Fermin",
+  salary: 2540,
+});
+colombia.push({
+  name: "Camila",
+  salary: 2138,
+});
+colombia.push({
+  name: "Marcos",
+  salary: 370,
+});
+colombia.push({
+  name: "Sebastian",
+  salary: 2450,
+});
+colombia.push({
+  name: "Maria",
+  salary: 950,
+});
+colombia.push({
+  name: "Will",
+  salary: 300,
+});
+colombia.push({
+  name: "Alexis",
+  salary: 750,
+});
+colombia.push({
+  name: "Erika",
+  salary: 800,
+});
+colombia.push({
+  name: "Yasmin",
+  salary: 400,
+});
